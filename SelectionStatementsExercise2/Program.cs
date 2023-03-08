837732addc578acd930e8191ec7f3ba21c811bdf
Victoria Pham <victoriapham52@gmail.com>
2023-03-08 13:06:59 -0800

    Switches Exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            var faveSubject = Console.ReadLine();
+
+            switch (faveSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is the same no matter where you go.");
+                    break;
+
+                case "science":
+                    Console.WriteLine("There's so much to learn.");
+                    break;
+
+                case "english":
+                    Console.WriteLine("Grammar is important.");
+                    break;
+
+                case "history":
+                    Console.WriteLine("You can't handle the future without the past.");
+                    break;
+
+                case "physical education":
+                    Console.WriteLine("Book smarts isn't the only thing in life.");
+                    break;
+
+                default:
+                    Console.WriteLine("Don't know her.");
+                    break;
+            }
+
         }
     }
 }
